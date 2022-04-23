89d92999898bde105a6a030118c5421870e29f9e
Daniel Bizari <dbizari@mulesoft.com>
2022-04-23 00:29:25 -0300

    add bbox to docker-compose

--- app/app.js
+++ app/app.js
@@ -7,6 +7,28 @@ app.get('/', (req, res) => {
     res.send('hello there');
 });
 
+app.get('/ping', (req, res) => {
+    res.send('pong');
+});
+
+app.get('/proxy-sync', (req, res) => {
+    // TODO: send request to sync BBOX 
+});
+
+app.get('/proxy-async', (req, res) => { 
+    // TODO: send request to async BBOX 
+});
+
+app.get('/heavy', (req, res) => {
+    let i = 0;
+
+    while(i < 5000000) {
+        i++;
+    }
+
+    res.send('done');
+});
+
 app.listen(PORT, () => {
     console.log(`listening on port ${PORT}`);
 });
